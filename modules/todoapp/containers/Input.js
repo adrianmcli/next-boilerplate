5408fb07235e98b64b7af986f5f54cd45f3f2852
Adrian Li <li.adrianmc@gmail.com>
2017-02-13 16:34:55 -0500

    added linting and fixed some linting errors, but not all

--- modules/todoapp/containers/Input.js
+++ modules/todoapp/containers/Input.js
@@ -2,8 +2,8 @@ import { connect } from 'react-redux'
 import { addTodo } from '../actions'
 import Component from '../components/Input'
 
-const mapDispatchToProps = (dispatch) => ({
-  addTodo: (todo) => dispatch(addTodo(todo)),
+const mapDispatchToProps = dispatch => ({
+  addTodo: todo => dispatch(addTodo(todo)),
 })
 
 export default connect(() => ({}), mapDispatchToProps)(Component)
--- modules/todoapp/containers/Todoapp.js
+++ modules/todoapp/containers/Todoapp.js
@@ -1,5 +1,4 @@
 import { connect } from 'react-redux'
-import { addTodo } from '../actions'
 import Component from '../components/Todoapp'
 
 export default connect(state => state.todoapp)(Component)
