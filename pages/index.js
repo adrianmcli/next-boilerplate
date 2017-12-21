a113e00a0fb09753df9131e9aec896f8348f4724
Adrian Li <li.adrianmc@gmail.com>
2017-12-20 23:57:59 -0800

    add styled-components

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,11 @@
-export default () => <div>Hello from Next.js</div>;
+import styled from "styled-components";
+
+const Red = styled.span`
+  color: red;
+`;
+
+export default () => (
+  <div>
+    <Red>Hello</Red> from Next.js
+  </div>
+);
